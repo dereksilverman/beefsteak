b7d1d30bb8812e848273dc6aca7363143aef1bba
Derek S <dereksilverman@gmail.com>
2014-02-20 09:20:14 -0500

    fixed forms and beyond

--- app.rb
+++ app.rb
@@ -18,14 +18,42 @@ module BrooklynBeefsteak
         erb :contact
     end
 
-
     post '/contact' do
+        full_name = params[:full_name]
+        email = params[:email]
+        message = params[:message]
+
+      m = Mandrill::API.new 'Y-FIsuPkJEwZ4jhI0hRiVQ'
+      message = {  
+       :subject=> "Beefsteak Signup - #{email} (#{full_name}) ", 
+       :from_name=> "Beef Dudes",  
+       :text=>"#{message}",  
+       :to=>[  
+         {  
+           :email=> "dereksilverman@gmail.com",  
+           :name=> "Derek Silverman" 
+         }  
+       ],  
+       :html=>"#{message}",  
+       :from_email=>"info@brooklynbeefsteak.com"  
+      }
+
+      sending = m.messages.send message  
+      puts sending
+    
+
+        erb :thankyou
+    end
+
+
+
+    post '/' do
       email = params[:email]
     
 
       m = Mandrill::API.new 'Y-FIsuPkJEwZ4jhI0hRiVQ'
       message = {  
-       :subject=> "Invitation Request - #{email} ", 
+       :subject=> "Beefsteak Signup - #{email} ", 
        :from_name=> "Beef Dudes",  
        :text=>"#{email} ",  
        :to=>[  
@@ -35,20 +63,19 @@ module BrooklynBeefsteak
          }  
        ],  
        :html=>"#{email}",  
-       :from_email=>"requests@crowdtangle.com"  
+       :from_email=>"info@brooklynbeefsteak.com"  
       }
 
       sending = m.messages.send message  
       puts sending
     
-     erb :contact
+     erb :thankyou
     end
 
     get '/thankyou' do
 
-     erb :thankyou
+        erb :thankyou
     end
-
 
 
     get '/history' do
